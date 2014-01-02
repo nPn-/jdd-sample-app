7bb33354ab0ac1fa9c85bd7aca97bbc277940f11
John Davis <npnster@gmail.com>
2014-01-02 00:12:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JddSampleApp::Application.config.secret_token = 'f4a2bda888ee4be71447bfe79e9792a804efb0cafe9a3abc5025cc204c723fa49438d09ff5c16bc4539abe7dd3531af62347b870c144b248049507a43121f995'
