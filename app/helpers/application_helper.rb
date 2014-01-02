588e3cd87312b8c23d0af2da526d916986309950
John Davis <npnster@gmail.com>
2014-01-02 14:04:22 -0500

    added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title)
+    title = "Ruby on Rails Tutorial Sample App"
+    title = page_title.empty? ? title  : "#{title} | #{page_title}"  
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
-  titles = ["Home", "Help", "About"]
+  titles = ["Home", "Help", "About", "Contacts"]
   
   titles.each do |title|
     describe "#{title} Page" do  
       it "should the correct h1 and title for the #{title} page" do
         visit "/static_pages/#{title.downcase}"
-        title_string = "Ruby on Rails Rutorial Sample App | #{title}"
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | #{title}")
+        title_string = "Ruby on Rails Tutorial Sample App"
+        title_string = "#{title_string} | #{title}" unless title == "Home" 
+        page.should have_selector('title', :text => title_string)
         page.should have_selector('h1', :text => title)
       end
     end
