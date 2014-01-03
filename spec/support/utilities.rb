8d7d9631708939f9285f64e679b62e6f76812e1f
John Davis <npnster@gmail.com>
2014-01-03 09:30:54 -0500

    exersises for chapter 5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,1 @@
-  def full_title(page_title)
-    title = "Ruby on Rails Tutorial Sample App"
-    title = page_title.empty? ? title  : "#{title} | #{page_title}"  
-  end
+include ApplicationHelper
